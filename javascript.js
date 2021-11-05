38b9da0ef1d65c8ff9a57a65adaee211dcf16f31
Boburjon <jboburjon2003@gmail.com>
2021-11-05 23:21:06 +0500

    Js

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var name = prompt("Write your name: ") 
+
+console.log("My name is : " + name);
+
+var age = prompt("How old are you: ")
+
+console.log("i am " + age + " years old");
+
+var q1 = prompt("Solve the example: 6 + 3 = ?")
+
+console.log("Example 1: 6 + 3 = 9 Your answer " + q1);
+
+var q2 = prompt("Solve the example: 20 - 5 = ? ")
+
+console.log("Example 2: 20 - 5 = 15 Your answer: " + q2);
+
+var q3 = prompt("Solve the example: 15 * 2 = ? ")
+
+console.log("Example 3: 15 * 2 = 30 Your answer " + q3);
+
+var q4 = prompt("Solve the example: 8 / 4 = ? ")
+
+console.log("Example 4: 8 / 4 = 2 Your answer " + q4);
+
+var q5 = prompt("Solve the example: 10 % 3 = ? ")
+
+console.log("Example 5: 10 % 3 = 1 Your answer " + q5);
+
+alert("If you want, you can see the answer:")
